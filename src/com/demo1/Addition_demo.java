6d6a495ac3867b89bdeb0a9d480d010143ab68d0
Mitali <kapuriamitali@gmail.com>
2020-04-13 18:34:50 -0700

    First commit

---
+++ src/com/demo1/Addition_demo.java
@@ -0,0 +1,11 @@
+package com.demo1;
+
+public class Addition_demo {
+
+	public static void main(String[] args) {
+
+		int a=4, b=6, result=0;
+		System.out.println(result=a+b);
+	}
+
+}
---
+++ src/com/demo2/Demo_print.java
@@ -0,0 +1,10 @@
+package com.demo2;
+
+public class Demo_print {
+
+	public static void main(String[] args) {
+
+		System.out.println("Successfully stored in the server.");
+	}
+
+}
